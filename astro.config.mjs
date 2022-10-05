fb584019e74ef6f4ba4b36f073b1e86a77507882
Jacob Carter <jacob@choicedigital.studio>
2022-10-05 11:11:33 +0100

    Added VUEJS to template.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image(), vue()]
 });
