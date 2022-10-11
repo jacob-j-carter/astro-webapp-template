9c14a07116d3611b3ae960d3a9787f7bd51e09d0
Jacob Carter <jacob@choicedigital.studio>
2022-10-11 12:37:34 +0100

    Removed default styling from anchor element

---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <a :href="HREF">
+    <button>
+      {{ content }}
+    </button>
+  </a>
+</template>
+
+
+<script lang="ts">
+
+export default {
+  name: 'Placeholder',
+  props: {
+    HREF: {
+      type: String,
+      default: '/'
+    },
+    content: {
+      type: String,
+      default: 'Click Me!'
+    },
+  }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+  @use '../sass/abstracts' as *;
+  @use '../sass/themes' as *;
+
+
+
+</style>
