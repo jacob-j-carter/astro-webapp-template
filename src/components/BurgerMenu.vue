3c56baeee0f4e418e2b605dabfa20e0f13f2bb34
Jacob Carter <jacob@choicedigital.studio>
2022-10-16 08:34:04 +0100

    Added menu components

---
+++ src/components/BurgerMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="lg:hidden bg-slate-900 text-white" :class="[isHidden ? 'nav-hidden' : 'nav-visible']">
+
+  </nav>
+</template>
+
+
+<script lang="ts">
+
+export default {
+  name: 'BurgerMenu',
+  props: {
+    isHidden: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+  @use '../sass/abstracts' as *;
+  @use '../sass/themes' as *;
+
+  nav {
+    height: 100vh;
+    width: 50%;
+  }
+
+</style>
