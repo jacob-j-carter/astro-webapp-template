8299301fdf27a56d549737871a4ecd46addb739b
Jacob Carter <jacob@choicedigital.studio>
2022-10-17 14:12:48 +0100

    Added vue iconify

--- src/components/BurgerMenu.vue
+++ src/components/BurgerMenu.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
+  <div class="w-screen">
+
+    <nav class="bg-stone-600 w-2/3 right-0 flex flex-col items-center justify-center fixed z-20">
+      
+      <Icon name="codicon:close" />
+
+    </nav>
 
   </div>
 </template>
 
 
 <script lang="ts">
+
+import { Icon } from 'astro-icon';
 
 export default {
   name: 'BurgerMenu'
@@ -18,6 +26,9 @@ export default {
   @use '../sass/abstracts' as *;
   @use '../sass/themes' as *;
 
+  nav {
+    height: 100vh;
+  }
 
 
 </style>
