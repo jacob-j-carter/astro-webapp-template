1c1a5e3e11f4319ce5b268e3f160da536f849129
Jacob Carter <jacob@choicedigital.studio>
2022-10-18 11:19:47 +0100

    added menu buttons as seperate component

---
+++ src/components/header/MobileButtons.vue
@@ -0,0 +1,51 @@
+<template>
+
+  <section class="flex justify-between items-center w-full p-2 mx-auto absolute z-10">
+
+  <div class="w-16 h-16 bg-stone-500 rounded-xl p-2">
+    <a href="/">
+      <img src="/favicon.svg" alt="logo">
+    </a>
+  </div>
+
+  <div class="w-16 h-16 bg-stone-500 rounded-xl p-2" @click="openMenu()">
+    <img src="/assets/images/icons/burgermenu.svg" alt="logo" class="w-full">
+  </div>
+
+  </section>
+
+</template>
+
+
+<script lang="ts">
+
+export default {
+  name: 'MobileButtons',
+  props: {
+
+  },
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+    openMenu() {
+      this.$emit('toggle-menu')
+    }
+  },
+  created () {
+
+  }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+  @use '../../sass/abstracts' as *;
+  @use '../../sass/themes' as *;
+
+
+
+</style>
--- src/components/header/MobileHeader.vue
+++ src/components/header/MobileHeader.vue
@@ -1,19 +1,7 @@
 <template>
   <div class="lg:hidden w-full">
 
-    <section class="flex justify-between items-center w-full p-2 mx-auto absolute z-10">
-
-      <div class="w-16 h-16 bg-stone-500 rounded-xl p-2">
-        <a href="/">
-          <img src="/favicon.svg" alt="logo">
-        </a>
-      </div>
-
-      <div class="w-16 h-16 bg-stone-500 rounded-xl p-2" @click="openMenu()">
-        <img src="/assets/images/icons/burgermenu.svg" alt="logo" class="w-full">
-      </div>
-
-    </section>
+    <MobileButtons @toggle-menu="openMenu()" />
 
     <BurgerMenu :navHiddenProp="navHiddenProp" client:load />
 
@@ -24,10 +12,11 @@
 
 <script lang="ts">
 import BurgerMenu from './BurgerMenu.vue';
+import MobileButtons from './MobileButtons.vue';
 
 export default {
     name: "MobileHeader",
-    components: { BurgerMenu },
+    components: { BurgerMenu, MobileButtons },
     data () {
       return {
         navHiddenProp: true
@@ -36,7 +25,6 @@ export default {
     methods: {
       openMenu() {
         this.navHiddenProp = !this.navHiddenProp
-        console.log("Yippie!")
       }
     },
 
