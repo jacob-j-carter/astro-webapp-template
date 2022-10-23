c6cd8b15d4bc8fa9fe1795dca31a4e5b98c32c9a
Jacob Carter <jacob@choicedigital.studio>
2022-10-23 14:49:16 +0100

    Fixed link to Montserrat

--- src/components/utility/ComingSoon.vue
+++ src/components/utility/ComingSoon.vue
@@ -1,8 +1,27 @@
 <template>
 
-  <div>
+  <main class="absolute top-0 left-0 bottom-0 right-0 flex items-center px-20">
 
-  </div>
+    <article class="w-2/3">
+      <div class="flex items-center">
+
+        <img src="/assets/images/photos/jacobcarterheadshot.png" alt="headshot of jacob carter" class="aspect-square rounded-full w-24">
+        <div class="ml-4">
+          <strong class="text-white m-0 font-size-22">
+            Jacob J. Carter
+          </strong>
+          <p class="text-white m-0 font-size-20">
+            Web Developer
+          </p>
+        </div>
+
+      </div>
+      <h2 class="text-white font-size-84 lg:w-2/3">
+        Our website is coming soon!
+      </h2>
+    </article>
+
+  </main>
 
 </template>
 
@@ -38,6 +57,17 @@ export default {
   @use '../../sass/abstracts' as *;
   @use '../../sass/themes' as *;
 
+  main {
+    z-index: 9999;
+    background-color: #0e0e0e;
+  }
 
+  h2 {
+    font-family: 'Felix', 'Montserrat' ,sans-serif;
+  }
+
+  strong, p {
+    font-family: 'Montserrat', sans-serif;
+  }
 
 </style>
