ee6fa779f91c9f2be068045489ccc866c429a44a
Jacob Carter <jacob@choicedigital.studio>
2022-11-11 08:49:13 +0000

    added custom tailwind fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,11 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			heading: ['"Felix"', 'serif'],
+			body: ['"Montserrat"', 'sans-serif'],
+			button: ['"Montserrat"', 'sans-serif']
+		}
 	},
 	plugins: [],
 }
